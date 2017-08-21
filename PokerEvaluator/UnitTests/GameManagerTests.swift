36acfc1c4a95b6566ca6eae9ab88ac48d1d3f7d0
Lin David, US-20 <David.Lin@bmwna.com>
2017-08-21 14:02:48 -0500

    add unit test

--- PokerEvaluator/UnitTests/GameManagerTests.swift
+++ PokerEvaluator/UnitTests/GameManagerTests.swift
@@ -25,9 +25,11 @@ class GameManagerTests {
     
     func runTests() {
         testStart()
+        testStart_NilGame()
     }
     
     func testStart() {
+        reset()
         mockGameDecoder.returnGame = []
         
         gameManager.start()
@@ -36,5 +38,24 @@ class GameManagerTests {
         assert(mockGameScorer.isScoreCalled == true, "should call gameScorer")
         assert(mockGameJudge.isGetSortedWinnerIdsCalled == true, "should call gameJudge")
         assert(mockConsole.isPrintCalled == true, "should call console print")
+    }
+    
+    func testStart_NilGame() {
+        reset()
+        mockGameDecoder.returnGame = nil
+        
+        gameManager.start()
+        
+        assert(mockGameDecoder.isDecodeGameFromConsoleInputCalled == true, "should call gameDecoder")
+        assert(mockGameScorer.isScoreCalled == false, "should not call gameScorer")
+        assert(mockGameJudge.isGetSortedWinnerIdsCalled == false, "should not call gameJudge")
+        assert(mockConsole.isPrintCalled == false, "should not call console print")
+    }
+    
+    func reset() {
+        mockGameDecoder.isDecodeGameFromConsoleInputCalled = false
+        mockGameScorer.isScoreCalled = false
+        mockGameJudge.isGetSortedWinnerIdsCalled = false
+        mockConsole.isPrintCalled = false
     }
 }
